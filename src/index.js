bc3e795ff94b2ff7bda50d82c1bf977a5076945e
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-10-20 03:53:25 -0400

    error implementation

--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };   //Only time for direct assignment
-    
-    window.navigator.geolocation.getCurrentPosition(   
+    this.state = { lat: null, errorMessage: "" };
+    //Only time for direct assignment
+
+    window.navigator.geolocation.getCurrentPosition(
       (position) =>
-        this.setState({   //This runs only after a value has been received in the future.
+        this.setState({
+          //This runs only after a value has been received in the future.
           lat: position.coords.latitude,
         }),
-      (error) => console.log(error)
+      (error) => this.setState({ errorMessage: error.message })
+      //Whenever we update state we don't have to update all values
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude: {this.state.lat} <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
