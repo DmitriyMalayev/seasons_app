51dad8ea0ba64b417f4bfe00e52574e59c870b36
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-10-21 04:03:49 -0400

    new object, refactored

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "It's so hot",
+    iconName: "sun",
+  },
+  winter: {
+    text: "It's freezing",
+    iconName: "snowflake",
+  },
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -10,11 +21,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season]; //contains text, iconName. Destructuring.
   return (
     <div>
-      <strong>{season === "winter" ? "It's freezing" : "It's so hot"}</strong>
+      <i className={`${iconName} icon`} />
+      <strong>{text}</strong>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
 
 export default SeasonDisplay;
+
+//Takes the value of icon and puts it in backticks along with the word icon
